25087256dc700ab4744ec6a091c856cec47bfa96
Richard Newman <richard@newmanworks.com>
2016-08-16 12:11:07 -0600

    Assignment 2: stack exercise

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -3,15 +3,27 @@ class MyStack
 
   def initialize
     @stack = Array.new
-    self.top = nil
+    refresh
   end
 
   def push(item)
+    @stack.unshift(item)   # or did you want me to do it via element numbers without using shift/unshift as in "@stack = [item] + @stack"
+    refresh
   end
 
   def pop
+    result = @stack.shift   # or did you want me to do it via element numbers without using shift/unshift as in "result = @stack[0]; @stack = @stack[1...-1]"
+    refresh
+    result
   end
 
   def empty?
+    @stack.length < 1
   end
+
+  def refresh
+    @top = @stack[0]
+  end
+  private :refresh
+
 end

865c7c520cf470c64366be5855b8589c76280708
Richard Newman <richard@newmanworks.com>
2016-08-16 12:06:44 -0600

    Assignment 2: queue exercise

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -4,15 +4,29 @@ class MyQueue
 
   def initialize
     @queue = Array.new
-    @head = @queue[0]
+    @tail = @head = @queue[0]
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element   # or @queue << element (but assignment prohibuts push for stack so avoiding here too)
+    refresh
+    @tail
   end
 
   def dequeue
+    result = @queue.shift  # or result = head; @queue = @queue[1..-1]
+    refresh
+    result
   end
 
   def empty?
+    @queue.length < 1
   end
+
+  def refresh
+    @head = @queue[-1]
+    @tail = @queue[0]
+  end
+  private :refresh
+
 end
